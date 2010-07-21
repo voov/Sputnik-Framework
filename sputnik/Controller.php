2661ad521bb21a9165a0f073b229ff405935d754
dande <daniel.fekete@voov.hu>
2010-07-21 18:14:31 +0200

    new Controller subsystem

--- sputnik/Controller.php
+++ sputnik/Controller.php
@@ -1,110 +1,110 @@
 <?php
 /**
  * Sputnik Controller
- * @version 2.0
- * @author Daniel Fekete - Voov Ltd.
- *
- * History:
- * 2.02 - Controller meg tud h�vni param�tereket is
- * 2.05 - minden Controller oszt�ly kap egy _autorun f�ggv�nyt, ami mindig lefut a main() el�tt
+ * @version 3.0
+ * @author Daniel Fekete
+ * @copyright VOOV Ltd.
  */
 
 require_once("Helper.php");
 require_once("Template.php");
 require_once("Sessions.php");
+require_once("Request.php");
 require_once("Db.php");
 require_once("PluginLoader.php");
+require_once("config/uri_mappings.php");
 
-
-/*	function __autoload($className) {
-
-		//exit;
-	}*/
 
 /**
- * Controller
- * @classDescription Minden app-t ebb�l az oszt�lyb�l kell �r�k�ltetni
- * @return
+ * URI Helper class
+ * @author Daniel Fekete
+ * @copyright 2010(c) VOOV Ltd.
+ */
+class URIHelper {
+	var $class_name = "";
+	var $class_path = array();
+	var $uri_array = array();
+	var $path_length = 0;
+	var $dir_path = "";
+
+	function  __construct() {
+		global $uri_mappings;
+		$uri_array = explode("/", $_SERVER["REQUEST_URI"]);
+		$uri_filtered = array_filter($uri_array, array($this, "remove_index"));
+		$uri = implode("/", $uri_filtered);
+		
+		foreach($uri_mappings as $map_regex => $map_replace) {
+			$uri = preg_replace("/^" . str_replace("/", "\/", $map_regex) . "$/", $map_replace, $uri); // Replace the URI using the mappings
+		}
+		
+		$this->uri_array = explode("/", $uri);
+		$this->get_class_path($this->uri_array);
+		$this->path_length = count($this->class_path);
+		$this->class_name = $this->uri_array[$this->path_length];
+		
+		$this->dir_path = implode("/", $this->class_path);
+	}
+	
+	private function remove_index($var) {
+		// strip out leading php controller files if present
+		if (strpos($var, ".php") !== false) return false;
+		else {
+			// do a quick standard URI check
+			// Framework 3 is VERY restrictive about URIs
+			if (preg_match('/^[a-zA-Z0-9_-][a-zA-Z0-9_-]*$/', $var)) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+	private function get_class_path($uri_array, $index=0) {
+		global $config;
+		$dir = $config["app_directory"] . implode("/", $this->class_path) . "/" .  $uri_array[$index];
+		if (is_dir($dir)) {
+			// Add to the class path
+			$this->class_path[] = $uri_array[$index];
+			// If the URI is longer
+			if (count($uri_array) > $index) {
+				$this->get_class_path($uri_array, $index+1); // Go one further
+			}
+		}
+	}
+
+}
+
+/**
+ * 
  */
 abstract class Controller {
 
-	private $start_from = 0;
-	private $o_requests = null;
 	public $view = null;
 	public $db = null;
-
+	public $uri_helper;
+	
 
 	function __construct() {
 		$this->view = Template::getInstance();
 		$this->db = Db::getInstance();
+
+		$this->uri_helper = new URIHelper();
 	}
 
-	/**
-	 * GetRequest
-	 * Visszaad egy objektumot, amiben a request-ek vannak t�rolva
-	 * P�lda: $this->GetRequest()->edit_id
-	 * @return session object
-	 */
+	public function GetURIPart($index) {
+		return $this->uri_helper->uri_array[$this->uri_helper->path_length+$index];
+	}
+
+	
 	public function GetRequest() {
-		if ($this->o_requests != null) return $this->o_requests;
-		else {
-			$o = (object)null;
-
-			// N�zz�k el�sz�r a rewrite-olt URL-eket
-			$req = $_SERVER["REQUEST_URI"];
-			$req_parts = explode("/", $req);
-			print_r($req_parts);
-
-			for($i = FrontController::$start_from-1; $i<count($req_parts); $i+=2)
-				if (!empty($req_parts[$i])) $o->{$req_parts[$i]} = $req_parts[$i+1];
-
-			foreach($_REQUEST as $req => $req_val) {
-				$o->{$req} = $req_val;
-			}
-			$this->o_requests = $o;
-			return ($o);
-		}
+		return Request::getInstance();
 	}
 
-	/**
-	 * GetRequestNumber
-	 * Visszaadja az �sszes GET request sz�m�t, amit rewrite-al adtunk meg, az index.php-t soha nem n�zi!
-	 * @return az �sszes GET request sz�m�t, amit rewrite-al adtunk meg
-	 */
-	public function GetNumberOfRequests() {
-
-		// N�zz�k el�sz�r a rewrite-olt URL-eket
-		$req = $_SERVER["REQUEST_URI"];
-		$req_parts = explode("/", $req);
-		$counter = 0;
-		foreach($req_parts as $req_part) {
-			if (strpos($req_part, ".php") === true) continue;
-			$counter++;
-		}
-
-		return $counter;
-	}
 
 	/**
-	 * GetRequestAt
-	 * Visszaad egy request string-et a megadott indexen
-	 * @return request string a megadott indexen
-	 * @param object $index a request indexe, az index.php-t soha nem n�zi!
-	 */
-	public function GetRequestAt($index) {
-
-		// N�zz�k el�sz�r a rewrite-olt URL-eket
-		$req = $_SERVER["REQUEST_URI"];
-		$req_parts = explode("/", $req);
-		if (strpos($req_parts[$index], ".php") === true && $this->GetNumberOfRequests() > 0) return $req_parts[$index+1];
-		elseif(strpos($req_parts[$index], ".php") === true && $this->GetNumberOfRequests() == 0) return null;
-		return $req_parts[$index];
-	}
-
-	/**
-	 *
-	 * @return
-	 * @param $name Object
+	 * Loads an external plugin
+	 * @return plugin object
+	 * @param $name the name of the plugin to load
+	 * @param $args optional arguments
 	 */
 	public function LoadPlugin($name, $args=array()) {
 		return PluginLoader::LoadPlugin($name, $this, $args);
@@ -112,53 +112,13 @@ abstract class Controller {
 
 
 	/**
-	 * GetSession
-	 * Visszaad egy objektumot, amiben a session-�k vannak t�rolva
-	 * P�lda: $this->GetSession()->username visszaadja a username session-t
-	 * P�lda: $this->GetSession()->username = "voov" be�ll�tja a username sessiont-t
+	 * Returns the session singelton object
 	 * @return session object
 	 */
 	public function GetSession() {
 		return Sessions::getInstance();
 	}
 
-	
-	/**
-	 * GetClassPath
-	 * Bels� f�ggv�ny, visszaadja a controllert �ltal k�rt oszt�lyt, a sz�ks�ges k�nyvt�rral
-	 * @return
-	 */
-	private function GetClassPath() {
-		$req = $_SERVER["REQUEST_URI"];
-		$req_parts = explode("/", $req);
-		$o = (object)null;
-
-		$dir_path = array();
-		// N�zz�k meg az �sszes request sort
-		foreach($req_parts as $r_part) {
-			// ha van benne egy .php, akkor valami.php lesz, l�phet�nk tov�bb
-			FrontController::$start_from++;
-			if (strpos($r_part, ".php") !== false) continue;
-
-			//TODO: m�ly k�nyvt�rkeres�s, mert �gy nem j�!
-			if (is_dir("apps/" . $r_part)) {
-				// k�nyvt�r
-				$dir_path[] = $r_part;
-				continue;
-			}
-
-			if (!isset($o->className))
-				$o->className = $r_part;
-			else {
-				$o->isMethod = $r_part;
-				break;
-			}
-
-		}
-
-		$o->dirPath = implode("/", $dir_path);
-		return $o;
-	}
 
 
 	/**
@@ -170,55 +130,50 @@ abstract class Controller {
 	 * @param $action Object[optional] melyik f�ggv�nyt h�vja meg az oszt�lyon bel�l
 	 */
 	protected function Forward($method = "", $action = "main") {
-
-		// tisztítsuk meg a $method-ot
-		$method = preg_replace("/\W/si", "", $method);
-
-		$cp = $this->GetClassPath();
-		if (empty($method)) {
-			$method = ucfirst($cp->className);
-		}
-
-		if (!isset($config["app_directory"])) $config["app_directory"] = "apps/";
-
-		if(!is_file($config["app_directory"] . $cp->dirPath . "/" . strtolower($method) . ".php")) {
-			die("Not class '$method'");
+		global $config;
+		// Get the filename
+		$class_filename = $config["app_directory"] . $this->uri_helper->dir_path . "/" . strtolower($this->uri_helper->class_name) . ".php";
+		if(!is_file($class_filename)) {
+			die("Not class '$method'"); // Error 404
 			error_log("Not class '$method'", 0);
 		}
 
-		require_once $config["app_directory"] . $cp->dirPath . "/" . strtolower($method) . ".php";
-		$controller = new $method();
+		require_once $class_filename;
+		$controller = new $this->uri_helper->class_name();
 
-		if (method_exists($controller, $cp->isMethod)) {
-			$action = $cp->isMethod;
-			FrontController::$start_from++;
+		$parameters_index = 1; //calc in class
+
+		if (method_exists($controller, $this->uri_helper->uri_array[$parameters_index])) {
+			$action = $this->uri_helper->uri_array[$parameters_index];
+			$parameters_index+=1;
 		}
 
-		// N�zz�k meg param�tereket is
+		if (!method_exists($controller, $action))
+			trigger_error("There is no '$action' in '$controller'!", E_USER_ERROR);
+		
 		$classReflect = new ReflectionClass($controller);
-
 		$classActionMethod = $classReflect->getMethod($action);
+
+		if ($classActionMethod->isPublic() != true)
+			trigger_error("'$action' is not public!", E_USER_ERROR);
+
 		if ($classActionMethod->getNumberOfParameters() > 0) {
 			$actionParameters = $classActionMethod->getParameters();
 			$paramCounter = -1;
 			$parameters = array();
 			foreach($actionParameters as $param) {
-
-				$val = $this->GetRequestAt(FrontController::$start_from+$paramCounter);
+				$val = $this->GetURIPart($paramCounter+$parameters_index);
 				$parameters[] = $val;
-
 				$paramCounter++;
 			}
-
-			FrontController::$start_from += $paramCounter+1;
 		}
 
-		if (!method_exists($controller, $action))
-			trigger_error("'$controller' oszt�lyban nem l�tezik '$action'!", E_USER_ERROR);
+		
 
-		// Ha l�tezik, h�vjuk meg az autorun f�ggv�nyt!
+		// Call the _autorun method if it exists
 		if (method_exists($controller, "_autorun"))
 			$controller->_autorun();
+		
 		call_user_func_array(array($controller, $action), $parameters);
 	}
 
