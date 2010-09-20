fc0aea8e8d5ea836bb17a7fca206c6ee7fc3fb8d
dande <daniel.fekete@voov.hu>
2010-09-20 16:10:24 +0200

    Singleton based class SetLocaleFromURI Added rewrite hook

--- sputnik/Localization.php
+++ sputnik/Localization.php
@@ -10,31 +10,62 @@
     @version 3.0 
     @author Daniel Fekete                          */
 
-require_once "config/localization.php";
-
 class Localization {
 
-	private static $loc;
+	private $loc;
+	static $instance = null;
+
+	/**
+	 *
+	 * @return <type> 
+	 */
+	public static function GetInstance() {
+		if(self::$instance === null) self::$instance = new Localization();
+		return self::$instance;
+	}
 
 	/** <autolink on>
 	    <aliasof TranslateText>
 	    
 	    Shortcut function to TranslateText */
 	public static function _($str, $args) {
-		return Localization::TranslateText($str, $args);
+		$o = Localization::GetInstance();
+		return $o->TranslateText($str, $args);
 	}
 
 	/** Translate text $str to currently set locale
 	    @param str   The string to translate
 	    @param args  optional arguments             */
-	public static function TranslateText($str, $args) {
+	public function TranslateText($str, $args) {
+		
 	}
 
 	/** Set the current locale
-	    @param loc_text  The locale setting. Example\: "en\-US" */
-	public static function SetLocale($loc_text) {
-		setlocale("LC_ALL", $loc_text);
-		Localization::$loc = $loc_text;
+	 */
+	public function SetLocale($loc) {
+		
+	}
+
+	public function RewriteHook($response, $host, $uri) {
+		//print_r(func_get_args());
+		return "http://" . $host . "/" . $this->loc ."/" . $uri;
+	}
+
+	public static function SetLocaleFromURI($regex, $uri) {
+		preg_match($regex, $uri, $matches);
+		// Query the selected language from database, and set the
+		// session
+		$lang = Db::GetInstance()->Query("SELECT * FROM languages WHERE code='{$matches[1]}'");
+		if($lang->Length() > 0)
+			Sessions::GetInstance()->lang_id = $lang->id; // If there is a language found
+		Sessions::GetInstance()->lang_code = $matches[1];
+		$localization = Localization::GetInstance();
+		$localization->loc = $matches[1];
+
+		// Add hook
+		Hooks::GetInstance()->RegisterFunction("pre_makeurl", array($localization, "RewriteHook"));
+
+		return $matches[2];
 	}
 }
 
